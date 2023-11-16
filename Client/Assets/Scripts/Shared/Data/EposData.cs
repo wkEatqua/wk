e4e82d1b6d9f888b7ca35f1a9326bb87bca888ab
cheonyongjun <yes1312@naver.com>
2023-11-17 01:26:09 +0900

    하드코딩된 수치 수정

--- Client/Assets/Scripts/Shared/Data/EposData.cs
+++ Client/Assets/Scripts/Shared/Data/EposData.cs
@@ -12,6 +12,8 @@ namespace Shared.Data
 	{
 		public long Level => data.Level;
 		public long TileNumber => data.TileNumber;
+		public float TileInterval => data.TileInterval;
+		public float TileScale => data.TileScale;
 
 		readonly EposLevel data;
 
@@ -22,7 +24,28 @@ namespace Shared.Data
 			StringBuilder sb = new StringBuilder();
 			sb.AppendLine($"Level : {Level}");
 			sb.AppendLine($"TileNumber : {TileNumber}");
+			sb.AppendLine($"TileInterval : {TileInterval}");
+			sb.AppendLine($"TileScale : {TileScale}");
 			return sb.ToString();
+		}
+	}
+
+	public class EposData : Database
+	{
+		public static IDictionary<long, EposLevelInfo> LevelDict = new Dictionary<long, EposLevelInfo>();
+		public override void ProcessDataLoad(string path)
+		{
+			LevelDict.Clear();
+			{
+				var levels = new Data<EposLevel>().GetData(path);
+
+				LevelDict = levels.ToDictionary(kv => kv.Level, kv => new EposLevelInfo(kv));
+			}
+		}
+
+		public static bool TryGetEposLevel(long Level, out EposLevelInfo info)
+		{
+			return LevelDict.TryGetValue(Level, out info);
 		}
 	}
 }

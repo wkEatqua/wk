8d5c30b4d6a0741584699ca1c9925705798957c4
cheonyongjun <yes1312@naver.com>
2023-12-12 14:10:03 +0900

    사운드 매니저

---
+++ Client/Assets/Scripts/Utils/SoundManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    float masterVolume = 1.0f;
+
+    float sfxVolume = 1.0f;
+
+    float bgmVolume = 1.0f;
+}
